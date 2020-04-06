06709122c0ed8ebbc25770edd6ac17161515e7e9
Emeric de Bernis <emeric.debernis@wifirst.fr>
2020-04-06 14:41:06 +0200

    implement JS build

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 PROTOC="${PROTOCBIN:-protoc}"
+GITPKG="${GITPKGBIN:-gitpkg}"
 
-$PROTOC -I=defs --python_out=build/python/sizematch/protobuf defs/items.proto
-#$PROTOC -I=defs --js_out=build/js defs/items.proto
-#$PROTOC -I=defs --go_out=build/go defs/items.proto
+# Python
+$PROTOC -I=defs --python_out=build/python/sizematch/protobuf defs/*.proto
+
+# JS
+$PROTOC -I=defs --js_out=import_style=commonjs,binary:build/js defs/*.proto
+( cd build/js && $GITPKG publish )
+
+# GO
+#$PROTOC -I=defs --go_out=build/go defs/*.proto
--- build/python/setup.py
+++ build/python/setup.py
@@ -6,6 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sizematch-protobuf",
+    version="0.0.1",
     packages=find_packages(),
     description="Sizematch protobuf libraries",
     author="Emeric de Bernis",
