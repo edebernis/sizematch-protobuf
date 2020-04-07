bee9eec6a68c17a0b80ff5c9076286ebfaccfe5a
Emeric de Bernis <emeric.debernis@wifirst.fr>
2020-04-07 11:24:19 +0200

    bugfix in build script

--- build.sh
+++ build.sh
@@ -16,15 +16,15 @@ SRC_DIR="defs"
 DST_DIR="build"
 
 # Python
-$PROTOC -I=SRC_DIR --python_out=$DST_DIR/python/sizematch/protobuf SRC_DIR/*/*.proto
+$PROTOC -I=$SRC_DIR --python_out=$DST_DIR/python/sizematch/protobuf $SRC_DIR/*/*.proto
 $GIT tag $VERSION
 $GIT push origin $VERSION
 
 # JS
-$PROTOC -I=SRC_DIR --js_out=import_style=commonjs,binary:$DST_DIR/js SRC_DIR/*/*.proto
+$PROTOC -I=$SRC_DIR --js_out=import_style=commonjs,binary:$DST_DIR/js $SRC_DIR/*/*.proto
 ( cd $DST_DIR/js && $GITPKG publish )
 
 # GO
-$PROTOC -I=SRC_DIR --go_opt=paths=source_relative --go_out=$DST_DIR/go SRC_DIR/*/*.proto
+$PROTOC -I=$SRC_DIR --go_opt=paths=source_relative --go_out=$DST_DIR/go $SRC_DIR/*/*.proto
 $GIT tag $DST_DIR/go/$VERSION
 $GIT push origin $DST_DIR/go/$VERSION
