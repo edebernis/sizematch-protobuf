c560f9667e7254be28bf7ee02168ced2e5115d4e
Emeric de Bernis <emeric.debernis@wifirst.fr>
2020-04-07 10:04:22 +0200

    improve build script

--- build.sh
+++ build.sh
@@ -3,16 +3,18 @@
 if [ -z "$1" ]
   then
     echo "No version supplied"
+    exit 1
 fi
 
 VERSION=$1
+GIT="${GITBIN:-git}"
 PROTOC="${PROTOCBIN:-protoc}"
 GITPKG="${GITPKGBIN:-gitpkg}"
 
 # Python
 $PROTOC -I=defs --python_out=build/python/sizematch/protobuf defs/*/*.proto
-git tag $VERSION
-git push origin $VERSION
+$GIT tag $VERSION
+$GIT push origin $VERSION
 
 # JS
 $PROTOC -I=defs --js_out=import_style=commonjs,binary:build/js defs/*/*.proto
@@ -20,5 +22,5 @@ $PROTOC -I=defs --js_out=import_style=commonjs,binary:build/js defs/*/*.proto
 
 # GO
 $PROTOC -I=defs --go_opt=paths=source_relative --go_out=build/go defs/*/*.proto
-git tag build/go/$VERSION
-git push origin build/go/$VERSION
+$GIT tag build/go/$VERSION
+$GIT push origin build/go/$VERSION
