16c2bcd78f9b070579d99299af761d229a745df5
Emeric de Bernis <emeric.debernis@wifirst.fr>
2020-04-07 09:15:34 +0200

    implement go build

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+VERSION=$1
 PROTOC="${PROTOCBIN:-protoc}"
 GITPKG="${GITPKGBIN:-gitpkg}"
 
 # Python
 $PROTOC -I=defs --python_out=build/python/sizematch/protobuf defs/*.proto
+git tag $VERSION
+git push origin $VERSION
 
 # JS
 $PROTOC -I=defs --js_out=import_style=commonjs,binary:build/js defs/*.proto
@@ -12,3 +15,5 @@ $PROTOC -I=defs --js_out=import_style=commonjs,binary:build/js defs/*.proto
 
 # GO
 $PROTOC -I=defs --go_out=build/go defs/*.proto
+git tag build/go/$VERSION
+git push origin build/go/$VERSION
