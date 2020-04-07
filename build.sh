6ac5c858c994eee530e4de2ab73b36cf3afe8e7c
Emeric de Bernis <emeric.debernis@wifirst.fr>
2020-04-07 11:59:50 +0200

    improve build script

--- build.sh
+++ build.sh
@@ -2,29 +2,41 @@
 
 if [ -z "$1" ]
   then
-    echo "No version supplied"
+    echo "No current version supplied. Semantic version format : vX.Y.Z"
+    exit 1
+fi
+if [ -z "$2" ]
+  then
+    echo "No new version supplied. Semantic version format : vX.Y.Z"
     exit 1
 fi
 
-VERSION=$1
+CURRENT_VERSION=$1
+NEW_VERSION=$2
 
 GIT="${GITBIN:-git}"
-PROTOC="${PROTOCBIN:-protoc}"
+BUMP2VERSION="${BUMP2VERSIONBIN:-bump2version}"
+NPM="${NPMBIN:-npm}"
 GITPKG="${GITPKGBIN:-gitpkg}"
+PROTOC="${PROTOCBIN:-protoc}"
 
 SRC_DIR="defs"
 DST_DIR="build"
 
-# Python
+# Generate protobuf code
 $PROTOC -I=$SRC_DIR --python_out=$DST_DIR/python/sizematch/protobuf $SRC_DIR/*/*.proto
-$GIT tag $VERSION
-$GIT push origin $VERSION
-
-# JS
 $PROTOC -I=$SRC_DIR --js_out=import_style=commonjs,binary:$DST_DIR/js $SRC_DIR/*/*.proto
-( cd $DST_DIR/js && $GITPKG publish )
-
-# GO
 $PROTOC -I=$SRC_DIR --go_opt=paths=source_relative --go_out=$DST_DIR/go $SRC_DIR/*/*.proto
-$GIT tag $DST_DIR/go/$VERSION
-$GIT push origin $DST_DIR/go/$VERSION
+
+# Bump versions
+$BUMP2VERSION --current-version $CURRENT_VERSION --new-version $NEW_VERSION patch $DST_DIR/python/setup.py
+( cd $DST_DIR/js && $NPM version $NEW_VERSION )
+$GIT commit -m "Bump version: ${CURRENT_VERSION} to ${NEW_VERSION}"
+
+# Tag using language-specific packaging syntax
+$GIT tag $NEW_VERSION
+$GIT tag $DST_DIR/go/$NEW_VERSION
+
+# Publish everything
+$GIT git push && $GIT push --tags
+( cd $DST_DIR/js && $GITPKG publish )
