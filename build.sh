d86954e7d440d448dd32df3f9e400bdd9a568972
Emeric de Bernis <emeric.debernis@wifirst.fr>
2020-04-12 10:11:55 +0200

    remove build dir

--- build.sh
+++ build.sh
@@ -21,23 +21,22 @@ GITPKG="${GITPKGBIN:-gitpkg}"
 PROTOC="${PROTOCBIN:-protoc}"
 
 SRC_DIR="defs"
-DST_DIR="build"
 
 # Generate protobuf code
-$PROTOC -I=$SRC_DIR --python_out=$DST_DIR/python/sizematch/protobuf $SRC_DIR/*/*.proto
-$PROTOC -I=$SRC_DIR --js_out=import_style=commonjs,binary:$DST_DIR/js $SRC_DIR/*/*.proto
-$PROTOC -I=$SRC_DIR --go_opt=paths=source_relative --go_out=$DST_DIR/go $SRC_DIR/*/*.proto
+$PROTOC -I=$SRC_DIR --python_out=python/sizematch/protobuf $SRC_DIR/*/*.proto
+$PROTOC -I=$SRC_DIR --js_out=import_style=commonjs,binary:js $SRC_DIR/*/*.proto
+$PROTOC -I=$SRC_DIR --go_opt=paths=source_relative --go_out=go $SRC_DIR/*/*.proto
 
 # Bump versions
-$BUMP2VERSION --allow-dirty --current-version $CURRENT_VERSION --new-version $NEW_VERSION patch $DST_DIR/python/setup.py
-( cd $DST_DIR/js && $NPM version $NEW_VERSION )
-$GIT add $DST_DIR/*
+$BUMP2VERSION --allow-dirty --current-version $CURRENT_VERSION --new-version $NEW_VERSION patch python/setup.py
+( cd js && $NPM version $NEW_VERSION )
+$GIT add python/* js/* go/*
 $GIT commit -m "Bump version: ${CURRENT_VERSION} to ${NEW_VERSION}"
 
 # Tag using language-specific packaging syntax
 $GIT tag $NEW_VERSION
-$GIT tag $DST_DIR/go/$NEW_VERSION
+$GIT tag go/$NEW_VERSION
 
 # Publish everything
 $GIT push && $GIT push --tags
-( cd $DST_DIR/js && $GITPKG publish )
+( cd js && $GITPKG publish )
